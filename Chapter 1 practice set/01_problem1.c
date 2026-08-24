d0d7ba54697f1e9dbcf30cb017abd6b224111c31
Anurag <anurag199251@gmail.com>
2026-08-24 22:31:24 +0530

    practice set problem 1 solution

---
+++ Chapter 1 practice set/01_problem1.c
@@ -0,0 +1,13 @@
+// 1. Write a C program to calculate the area of a rectangle:
+// a. Using hard coded inputs.
+// b. Using inputs supplied by the user.
+
+#include <stdio.h>
+
+int main(){
+        int length = 22;
+        int width = 10;
+        int area = length * width;
+        printf("Area of rectangle with hard coded inputs: %d\n", area);
+    return 0;
+}
