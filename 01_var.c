194a7e1b25bc48e22170f4395f732c5d339f1f51
Anurag <anurag199251@gmail.com>
2026-08-25 11:49:56 +0530

    changed the definaion for variables

--- 01_var.c
+++ 01_var.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    int i = 10; // variables are the memory stored in the computer and the compiler saves the memory location with the variable name, so we can use that variable name to access the value stored in that memory location.
+    int i = 10; // At its core, a variable is a human-friendly name mapped to a physical address in the computer's RAM. It gives you a way to store, track, and modify binary data without manually managing raw memory addresses.
     int j = i;
     // %d is format specifier for integer values and it also works as a placeholder for the variable values, where we write %d we can get that variable value in the output, from left to right, the first %d will be replaced by the value of i and the second %d will be replaced by the value of j.  
     printf("The value of i is %d and value of j is %d", i, j);
