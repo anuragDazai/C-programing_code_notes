85f5574bc62c96ca8f08598d3d3d1c8cad29040d
Anurag <anurag199251@gmail.com>
2026-09-07 22:06:43 +0530

    all problems done chapter 6 ps

--- Chapter 6 practice set/06_problem7.c
+++ Chapter 6 practice set/06_problem7.c
@@ -4,9 +4,9 @@
 
 #include <stdio.h>
 
-void multiplyByTen(int* ptr)
+void multiplyByTen(int ptr)
 {
-    *ptr = (*ptr) * 10; 
+    ptr = (ptr) * 10; 
 }
 
 int main(void)
@@ -15,9 +15,9 @@ int main(void)
 
     printf("Value before function call: %d\n", num);
 
-    multiplyByTen(&num); 
+    multiplyByTen(num); 
 
-    printf("Value after function call:  %d\n", num); 
+    printf("Value after function call:  %d\n", num); // we are giving only the copy of the variable not the address of the variable hence we cannot change the value of the variable in the main function
 
     return 0;
 }
